1429710d8356947af90fce3dd04b8e18bac41dd8
robkinson94 <129741120+robkinson94@users.noreply.github.com>
2024-12-06 16:51:24 +0000

    vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  server: {
-    proxy: {
-      '/api': 'http://127.0.0.1:8000', // Proxy API requests during development
+    plugins: [react()],
+    build: {
+        outDir: 'dist', // Ensure the build directory matches Render's default publish directory
     },
-  },
+    server: {
+        port: 3000,
+    },
 });
